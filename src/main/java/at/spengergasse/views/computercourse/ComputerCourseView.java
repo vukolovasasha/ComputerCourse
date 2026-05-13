4a7ee7064b8ffc8f7b206985906c47a95535b806
start.vaadin.com <no-reply@vaadin.com>
2026-05-13 08:36:35 +0000

    Generated project

---
+++ src/main/java/at/spengergasse/views/computercourse/ComputerCourseView.java
@@ -0,0 +1,35 @@
+package at.spengergasse.views.computercourse;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("ComputerCourse")
+@Route("")
+@Menu(order = 0, icon = LineAwesomeIconUrl.HOME_SOLID)
+public class ComputerCourseView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public ComputerCourseView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
---
+++ src/main/java/at/spengergasse/views/lessons/LessonsView.java
@@ -0,0 +1,36 @@
+package at.spengergasse.views.lessons;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import org.vaadin.lineawesome.LineAwesomeIconUrl;
+
+@PageTitle("lessons")
+@Route("about")
+@Menu(order = 1, icon = LineAwesomeIconUrl.GRADUATION_CAP_SOLID)
+public class LessonsView extends VerticalLayout {
+
+    public LessonsView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        H2 header = new H2("This place intentionally left empty");
+        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
+        add(header);
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
